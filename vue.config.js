0c348338566caa1973ee511046bc559b3c1207aa
tai0525 <whoareu525@gmail.com>
2022-10-27 10:48:27 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-teashop/" : "/",
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = "沁記茶行";
